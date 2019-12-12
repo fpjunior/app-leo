15d46fb9cb94adedfb0ea459c27bdc386a337a0b
fpjunior <fpjunior@gmail.com>
2019-12-12 15:09:42 -0300

    ultima atualização

--- src/app/primeiro-componente/primeiro-componente.component.ts
+++ src/app/primeiro-componente/primeiro-componente.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BrowserTransferStateModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-primeiro-componente',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrimeiroComponenteComponent implements OnInit {
 
+meutitulo = "texto da variável meutitulo!!!"
+texto1= " texto da variável texto1"
+texto2 = "texto da variável texto2"
+
+cliente = {
+  id: 1,
+  nome: "fulano", 
+}
+
+clientes = [
+  {
+    id:2,
+    nome: "beltrano",
+    ativo: true
+  },
+  {
+    id:3,
+    nome: "Fernando",
+    ativo: false
+  },
+  {
+    id:4,
+    nome: "ciclano",
+    ativo: true
+  }
+]
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  geraTexto(){
+    return "Texto da função geraTexto()";
   }
 
 }
